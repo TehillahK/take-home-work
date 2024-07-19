94ee1cc7dd533476b9ab9a78a64c5c402b55a378
Tehillahk <tehillahk@outlook.com>
2024-07-19 07:12:25 -0500

    added interface

--- frontend/src/pages/home/components/Listing.tsx
+++ frontend/src/pages/home/components/Listing.tsx
@@ -1,7 +1,20 @@
-const StoreListing = () => {
+import Product from "../../../interfaces/Product";
+
+const StoreListing = ({products}:{products: Product[]}) => {
     return ( 
         <div>
-            
+            <ul>
+                {
+                    products?.map(
+                        (product:Product)=>(
+                            <div key={product.id}>
+                                {product.name}
+                            </div>
+                        )
+                    )
+                }
+            </ul>
+
         </div>
      );
 }
