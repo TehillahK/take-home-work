8be1afa6fab2f7289626af38ec5588cd75205664
Tehillahk <tehillahk@outlook.com>
2024-07-19 07:42:48 -0500

    populate grid

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,11 +1,28 @@
+import Product from "../../interfaces/Product";
 import Navbar from "../../Utils/Navbar";
 import Hero from "./components/Hero";
+import StoreListing from "./components/Listing";
 
 const HomePage = () => {
+    const productList:Product[] = [
+        {
+            id:0,
+            name:"Test 1",
+            photoUrl:"",
+            price:25
+        },
+        {
+            id:0,
+            name:"Test 2",
+            photoUrl:"",
+            price:10
+        }
+    ]
     return (
         <>
             <Navbar />
             <Hero />
+            <StoreListing products={productList}/>
         </>
     );
 }
--- frontend/src/pages/home/components/Listing.tsx
+++ frontend/src/pages/home/components/Listing.tsx
@@ -1,22 +1,28 @@
 import Product from "../../../interfaces/Product";
+import ProductCard from "../../../Utils/ProductCard";
 
-const StoreListing = ({products}:{products: Product[]}) => {
-    return ( 
-        <div>
-            <ul>
+const StoreListing = ({ products }: { products: Product[] }) => {
+    return (
+        <div className="px-10 py-10">
+            <ul className="grid grid-cols-3">
                 {
                     products?.map(
-                        (product:Product)=>(
-                            <div key={product.id}>
-                                {product.name}
-                            </div>
+                        (product: Product) => (
+                            <ProductCard
+                                id={product.id}
+                                name={product.name}
+                                price={product.price}
+                                photoUrl={product.photoUrl}
+                            />
+
+
                         )
                     )
                 }
             </ul>
 
         </div>
-     );
+    );
 }
- 
+
 export default StoreListing;
